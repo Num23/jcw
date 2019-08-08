44ddecfad8e5d3aac171620c35be9d00549e024a
Num23 <1660166863@qq.com>
2019-08-08 20:48:34 +0800

    修改的内容

--- jcw/web/src/main/java/com/example/controller/jcwApplication.java
+++ jcw/web/src/main/java/com/example/controller/jcwApplication.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Classname com.example.jcwApplication
@@ -11,6 +13,8 @@ import org.springframework.boot.SpringBootConfiguration;
  * @Created by lwd
  */
 @SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan("com.example.controller")
 public class jcwApplication {
 
     public static void main(String[] args) {
