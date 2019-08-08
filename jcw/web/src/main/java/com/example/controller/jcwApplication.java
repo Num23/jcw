101eba014cd90872ef6274a08f25ca6802f92ca9
luwendong <luwendong@thunisoft.com>
2019-08-08 18:30:38 +0800

    jcwProject

---
+++ jcw/web/src/main/java/com/example/controller/jcwApplication.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * @Classname com.example.jcwApplication
+ * @Description TODO
+ * @version:1.0
+ * @Date 2019/8/8 18:10
+ * @Created by lwd
+ */
+@SpringBootConfiguration
+public class jcwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(jcwApplication.class, args);
+    }
+}
